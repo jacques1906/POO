7e3eb58cdeb53c47ac5b17d31243df42b81491c4
Jacques ZHU <jacques.zhu@viacesi.fr>
2022-11-30 10:02:36 +0100

    Update(dashboard)

--- Dashboard.h
+++ Dashboard.h
@@ -38,6 +38,17 @@ namespace POO {
 	protected:
 	private: System::Windows::Forms::Panel^ panel;
 	private: System::Windows::Forms::Label^ Title_Dashboard;
+	private: System::Windows::Forms::Panel^ panel2;
+	private: System::Windows::Forms::Button^ Btn_Gestion_stat;
+
+	private: System::Windows::Forms::Button^ Btn_Gestion_stock;
+
+	private: System::Windows::Forms::Button^ btn_Gestion_commandes;
+
+	private: System::Windows::Forms::Button^ Btn_Gestion_clients;
+
+	private: System::Windows::Forms::Button^ Btn_Gestion_Personnel;
+
 
 	private:
 		/// <summary>
@@ -55,24 +66,41 @@ namespace POO {
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
 			this->panel = (gcnew System::Windows::Forms::Panel());
 			this->Title_Dashboard = (gcnew System::Windows::Forms::Label());
+			this->panel2 = (gcnew System::Windows::Forms::Panel());
+			this->Btn_Gestion_Personnel = (gcnew System::Windows::Forms::Button());
+			this->Btn_Gestion_clients = (gcnew System::Windows::Forms::Button());
+			this->btn_Gestion_commandes = (gcnew System::Windows::Forms::Button());
+			this->Btn_Gestion_stock = (gcnew System::Windows::Forms::Button());
+			this->Btn_Gestion_stat = (gcnew System::Windows::Forms::Button());
+			this->panel1->SuspendLayout();
 			this->panel->SuspendLayout();
 			this->SuspendLayout();
 			// 
 			// panel1
 			// 
+			this->panel1->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(24)), static_cast<System::Int32>(static_cast<System::Byte>(30)),
+				static_cast<System::Int32>(static_cast<System::Byte>(54)));
+			this->panel1->Controls->Add(this->Btn_Gestion_stat);
+			this->panel1->Controls->Add(this->Btn_Gestion_stock);
+			this->panel1->Controls->Add(this->btn_Gestion_commandes);
+			this->panel1->Controls->Add(this->Btn_Gestion_clients);
+			this->panel1->Controls->Add(this->Btn_Gestion_Personnel);
+			this->panel1->Controls->Add(this->panel2);
 			this->panel1->Dock = System::Windows::Forms::DockStyle::Left;
 			this->panel1->Location = System::Drawing::Point(0, 0);
+			this->panel1->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(244, 649);
+			this->panel1->Size = System::Drawing::Size(488, 1248);
 			this->panel1->TabIndex = 0;
 			// 
 			// panel
 			// 
 			this->panel->Controls->Add(this->Title_Dashboard);
 			this->panel->Dock = System::Windows::Forms::DockStyle::Top;
-			this->panel->Location = System::Drawing::Point(244, 0);
+			this->panel->Location = System::Drawing::Point(488, 0);
+			this->panel->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
 			this->panel->Name = L"panel";
-			this->panel->Size = System::Drawing::Size(983, 207);
+			this->panel->Size = System::Drawing::Size(1966, 176);
 			this->panel->TabIndex = 1;
 			// 
 			// Title_Dashboard
@@ -81,22 +109,112 @@ namespace POO {
 				| System::Windows::Forms::AnchorStyles::Right));
 			this->Title_Dashboard->Font = (gcnew System::Drawing::Font(L"Microsoft Sans Serif", 27.75F, System::Drawing::FontStyle::Regular,
 				System::Drawing::GraphicsUnit::Point, static_cast<System::Byte>(0)));
-			this->Title_Dashboard->Location = System::Drawing::Point(232, 69);
+			this->Title_Dashboard->Location = System::Drawing::Point(464, 45);
+			this->Title_Dashboard->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
 			this->Title_Dashboard->Name = L"Title_Dashboard";
-			this->Title_Dashboard->Size = System::Drawing::Size(546, 55);
+			this->Title_Dashboard->Size = System::Drawing::Size(1092, 106);
 			this->Title_Dashboard->TabIndex = 0;
 			this->Title_Dashboard->Text = L"Dashboard";
 			this->Title_Dashboard->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			// 
+			// panel2
+			// 
+			this->panel2->Dock = System::Windows::Forms::DockStyle::Top;
+			this->panel2->Location = System::Drawing::Point(0, 0);
+			this->panel2->Name = L"panel2";
+			this->panel2->Size = System::Drawing::Size(488, 176);
+			this->panel2->TabIndex = 0;
+			// 
+			// Btn_Gestion_Personnel
+			// 
+			this->Btn_Gestion_Personnel->Dock = System::Windows::Forms::DockStyle::Top;
+			this->Btn_Gestion_Personnel->FlatAppearance->BorderSize = 0;
+			this->Btn_Gestion_Personnel->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->Btn_Gestion_Personnel->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 13.875F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->Btn_Gestion_Personnel->ForeColor = System::Drawing::Color::Gainsboro;
+			this->Btn_Gestion_Personnel->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->Btn_Gestion_Personnel->Location = System::Drawing::Point(0, 176);
+			this->Btn_Gestion_Personnel->Name = L"Btn_Gestion_Personnel";
+			this->Btn_Gestion_Personnel->Size = System::Drawing::Size(488, 131);
+			this->Btn_Gestion_Personnel->TabIndex = 1;
+			this->Btn_Gestion_Personnel->Text = L"Gestion personnel";
+			this->Btn_Gestion_Personnel->UseVisualStyleBackColor = true;
+			// 
+			// Btn_Gestion_clients
+			// 
+			this->Btn_Gestion_clients->Dock = System::Windows::Forms::DockStyle::Top;
+			this->Btn_Gestion_clients->FlatAppearance->BorderSize = 0;
+			this->Btn_Gestion_clients->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->Btn_Gestion_clients->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 13.875F, System::Drawing::FontStyle::Bold));
+			this->Btn_Gestion_clients->ForeColor = System::Drawing::Color::Gainsboro;
+			this->Btn_Gestion_clients->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->Btn_Gestion_clients->Location = System::Drawing::Point(0, 307);
+			this->Btn_Gestion_clients->Name = L"Btn_Gestion_clients";
+			this->Btn_Gestion_clients->Size = System::Drawing::Size(488, 131);
+			this->Btn_Gestion_clients->TabIndex = 2;
+			this->Btn_Gestion_clients->Text = L"Gestion clients";
+			this->Btn_Gestion_clients->UseVisualStyleBackColor = true;
+			// 
+			// btn_Gestion_commandes
+			// 
+			this->btn_Gestion_commandes->Dock = System::Windows::Forms::DockStyle::Top;
+			this->btn_Gestion_commandes->FlatAppearance->BorderSize = 0;
+			this->btn_Gestion_commandes->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->btn_Gestion_commandes->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 13.875F, System::Drawing::FontStyle::Bold));
+			this->btn_Gestion_commandes->ForeColor = System::Drawing::Color::Gainsboro;
+			this->btn_Gestion_commandes->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->btn_Gestion_commandes->Location = System::Drawing::Point(0, 438);
+			this->btn_Gestion_commandes->Name = L"btn_Gestion_commandes";
+			this->btn_Gestion_commandes->Size = System::Drawing::Size(488, 131);
+			this->btn_Gestion_commandes->TabIndex = 3;
+			this->btn_Gestion_commandes->Text = L"Gestion commandes";
+			this->btn_Gestion_commandes->UseVisualStyleBackColor = true;
+			// 
+			// Btn_Gestion_stock
+			// 
+			this->Btn_Gestion_stock->Dock = System::Windows::Forms::DockStyle::Top;
+			this->Btn_Gestion_stock->FlatAppearance->BorderSize = 0;
+			this->Btn_Gestion_stock->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->Btn_Gestion_stock->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 13.875F, System::Drawing::FontStyle::Bold));
+			this->Btn_Gestion_stock->ForeColor = System::Drawing::Color::Gainsboro;
+			this->Btn_Gestion_stock->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->Btn_Gestion_stock->Location = System::Drawing::Point(0, 569);
+			this->Btn_Gestion_stock->Name = L"Btn_Gestion_stock";
+			this->Btn_Gestion_stock->Size = System::Drawing::Size(488, 131);
+			this->Btn_Gestion_stock->TabIndex = 4;
+			this->Btn_Gestion_stock->Text = L"Gestion stock";
+			this->Btn_Gestion_stock->UseVisualStyleBackColor = true;
+			// 
+			// Btn_Gestion_stat
+			// 
+			this->Btn_Gestion_stat->Dock = System::Windows::Forms::DockStyle::Top;
+			this->Btn_Gestion_stat->FlatAppearance->BorderSize = 0;
+			this->Btn_Gestion_stat->FlatStyle = System::Windows::Forms::FlatStyle::Flat;
+			this->Btn_Gestion_stat->Font = (gcnew System::Drawing::Font(L"Nirmala UI", 13.875F, System::Drawing::FontStyle::Bold));
+			this->Btn_Gestion_stat->ForeColor = System::Drawing::Color::Gainsboro;
+			this->Btn_Gestion_stat->ImageAlign = System::Drawing::ContentAlignment::MiddleLeft;
+			this->Btn_Gestion_stat->Location = System::Drawing::Point(0, 700);
+			this->Btn_Gestion_stat->Name = L"Btn_Gestion_stat";
+			this->Btn_Gestion_stat->Size = System::Drawing::Size(488, 131);
+			this->Btn_Gestion_stat->TabIndex = 5;
+			this->Btn_Gestion_stat->Text = L"Gestion statistiques";
+			this->Btn_Gestion_stat->UseVisualStyleBackColor = true;
+			// 
 			// Dashboard
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
-			this->ClientSize = System::Drawing::Size(1227, 649);
+			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(46)), static_cast<System::Int32>(static_cast<System::Byte>(51)),
+				static_cast<System::Int32>(static_cast<System::Byte>(73)));
+			this->ClientSize = System::Drawing::Size(2454, 1248);
 			this->Controls->Add(this->panel);
 			this->Controls->Add(this->panel1);
+			this->FormBorderStyle = System::Windows::Forms::FormBorderStyle::None;
+			this->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
 			this->Name = L"Dashboard";
 			this->Text = L"Dashboard";
+			this->panel1->ResumeLayout(false);
 			this->panel->ResumeLayout(false);
 			this->ResumeLayout(false);
 
--- login.h
+++ login.h
@@ -64,11 +64,11 @@ namespace POO {
 		{
 			System::ComponentModel::ComponentResourceManager^ resources = (gcnew System::ComponentModel::ComponentResourceManager(login::typeid));
 			this->panel1 = (gcnew System::Windows::Forms::Panel());
-			this->label1 = (gcnew System::Windows::Forms::Label());
-			this->write_Username = (gcnew Bunifu::Framework::UI::BunifuTextbox());
-			this->write_password = (gcnew Bunifu::Framework::UI::BunifuTextbox());
-			this->btn_login = (gcnew Bunifu::Framework::UI::BunifuThinButton2());
 			this->btn_cancel = (gcnew Bunifu::Framework::UI::BunifuThinButton2());
+			this->btn_login = (gcnew Bunifu::Framework::UI::BunifuThinButton2());
+			this->write_password = (gcnew Bunifu::Framework::UI::BunifuTextbox());
+			this->write_Username = (gcnew Bunifu::Framework::UI::BunifuTextbox());
+			this->label1 = (gcnew System::Windows::Forms::Label());
 			this->panel1->SuspendLayout();
 			this->SuspendLayout();
 			// 
@@ -80,82 +80,11 @@ namespace POO {
 			this->panel1->Controls->Add(this->write_password);
 			this->panel1->Controls->Add(this->write_Username);
 			this->panel1->Controls->Add(this->label1);
-			this->panel1->Location = System::Drawing::Point(472, 24);
+			this->panel1->Location = System::Drawing::Point(944, 46);
+			this->panel1->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
 			this->panel1->Name = L"panel1";
-			this->panel1->Size = System::Drawing::Size(417, 646);
+			this->panel1->Size = System::Drawing::Size(834, 1242);
 			this->panel1->TabIndex = 0;
-			// 
-			// label1
-			// 
-			this->label1->AutoSize = true;
-			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI Black", 27.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->label1->Location = System::Drawing::Point(144, 37);
-			this->label1->Name = L"label1";
-			this->label1->Size = System::Drawing::Size(140, 50);
-			this->label1->TabIndex = 0;
-			this->label1->Text = L"LOGIN";
-			this->label1->Click += gcnew System::EventHandler(this, &login::Write_Username);
-			this->label1->Click += gcnew System::EventHandler(this, &login::Write_Password);
-			// 
-			// write_Username
-			// 
-			this->write_Username->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
-				static_cast<System::Int32>(static_cast<System::Byte>(64)));
-			this->write_Username->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"write_Username.BackgroundImage")));
-			this->write_Username->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
-			this->write_Username->ForeColor = System::Drawing::Color::MintCream;
-			this->write_Username->Icon = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"write_Username.Icon")));
-			this->write_Username->Location = System::Drawing::Point(81, 186);
-			this->write_Username->Name = L"write_Username";
-			this->write_Username->Size = System::Drawing::Size(250, 42);
-			this->write_Username->TabIndex = 1;
-			this->write_Username->text = L"Username";
-			this->write_Username->OnTextChange += gcnew System::EventHandler(this, &login::Write_Username);
-			// 
-			// write_password
-			// 
-			this->write_password->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
-				static_cast<System::Int32>(static_cast<System::Byte>(64)));
-			this->write_password->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"write_password.BackgroundImage")));
-			this->write_password->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
-			this->write_password->ForeColor = System::Drawing::Color::MintCream;
-			this->write_password->Icon = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"write_password.Icon")));
-			this->write_password->Location = System::Drawing::Point(81, 309);
-			this->write_password->Name = L"write_password";
-			this->write_password->Size = System::Drawing::Size(250, 42);
-			this->write_password->TabIndex = 2;
-			this->write_password->text = L"password";
-			this->write_password->OnTextChange += gcnew System::EventHandler(this, &login::Write_Password);
-			// 
-			// btn_login
-			// 
-			this->btn_login->ActiveBorderThickness = 1;
-			this->btn_login->ActiveCornerRadius = 20;
-			this->btn_login->ActiveFillColor = System::Drawing::Color::White;
-			this->btn_login->ActiveForecolor = System::Drawing::Color::MidnightBlue;
-			this->btn_login->ActiveLineColor = System::Drawing::Color::White;
-			this->btn_login->AllowDrop = true;
-			this->btn_login->BackColor = System::Drawing::Color::Transparent;
-			this->btn_login->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"btn_login.BackgroundImage")));
-			this->btn_login->ButtonText = L"Login";
-			this->btn_login->Cursor = System::Windows::Forms::Cursors::Hand;
-			this->btn_login->Font = (gcnew System::Drawing::Font(L"Century Gothic", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
-				static_cast<System::Byte>(0)));
-			this->btn_login->ForeColor = System::Drawing::Color::Honeydew;
-			this->btn_login->IdleBorderThickness = 1;
-			this->btn_login->IdleCornerRadius = 20;
-			this->btn_login->IdleFillColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
-				static_cast<System::Int32>(static_cast<System::Byte>(64)));
-			this->btn_login->IdleForecolor = System::Drawing::Color::White;
-			this->btn_login->IdleLineColor = System::Drawing::Color::White;
-			this->btn_login->Location = System::Drawing::Point(15, 434);
-			this->btn_login->Margin = System::Windows::Forms::Padding(5);
-			this->btn_login->Name = L"btn_login";
-			this->btn_login->Size = System::Drawing::Size(181, 41);
-			this->btn_login->TabIndex = 3;
-			this->btn_login->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
-			this->btn_login->Click += gcnew System::EventHandler(this, &login::Click_Btn_Login);
 			// 
 			// btn_cancel
 			// 
@@ -178,22 +107,98 @@ namespace POO {
 				static_cast<System::Int32>(static_cast<System::Byte>(64)));
 			this->btn_cancel->IdleForecolor = System::Drawing::Color::White;
 			this->btn_cancel->IdleLineColor = System::Drawing::Color::White;
-			this->btn_cancel->Location = System::Drawing::Point(206, 434);
-			this->btn_cancel->Margin = System::Windows::Forms::Padding(5);
+			this->btn_cancel->Location = System::Drawing::Point(412, 835);
+			this->btn_cancel->Margin = System::Windows::Forms::Padding(10, 10, 10, 10);
 			this->btn_cancel->Name = L"btn_cancel";
-			this->btn_cancel->Size = System::Drawing::Size(181, 41);
+			this->btn_cancel->Size = System::Drawing::Size(362, 79);
 			this->btn_cancel->TabIndex = 4;
 			this->btn_cancel->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
 			this->btn_cancel->Click += gcnew System::EventHandler(this, &login::Click_Btn_Cancel);
 			// 
+			// btn_login
+			// 
+			this->btn_login->ActiveBorderThickness = 1;
+			this->btn_login->ActiveCornerRadius = 20;
+			this->btn_login->ActiveFillColor = System::Drawing::Color::White;
+			this->btn_login->ActiveForecolor = System::Drawing::Color::MidnightBlue;
+			this->btn_login->ActiveLineColor = System::Drawing::Color::White;
+			this->btn_login->AllowDrop = true;
+			this->btn_login->BackColor = System::Drawing::Color::Transparent;
+			this->btn_login->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"btn_login.BackgroundImage")));
+			this->btn_login->ButtonText = L"Login";
+			this->btn_login->Cursor = System::Windows::Forms::Cursors::Hand;
+			this->btn_login->Font = (gcnew System::Drawing::Font(L"Century Gothic", 12, System::Drawing::FontStyle::Regular, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->btn_login->ForeColor = System::Drawing::Color::Honeydew;
+			this->btn_login->IdleBorderThickness = 1;
+			this->btn_login->IdleCornerRadius = 20;
+			this->btn_login->IdleFillColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->btn_login->IdleForecolor = System::Drawing::Color::White;
+			this->btn_login->IdleLineColor = System::Drawing::Color::White;
+			this->btn_login->Location = System::Drawing::Point(30, 835);
+			this->btn_login->Margin = System::Windows::Forms::Padding(10, 10, 10, 10);
+			this->btn_login->Name = L"btn_login";
+			this->btn_login->Size = System::Drawing::Size(362, 79);
+			this->btn_login->TabIndex = 3;
+			this->btn_login->TextAlign = System::Drawing::ContentAlignment::MiddleCenter;
+			this->btn_login->Click += gcnew System::EventHandler(this, &login::Click_Btn_Login);
+			// 
+			// write_password
+			// 
+			this->write_password->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->write_password->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"write_password.BackgroundImage")));
+			this->write_password->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->write_password->ForeColor = System::Drawing::Color::MintCream;
+			this->write_password->Icon = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"write_password.Icon")));
+			this->write_password->Location = System::Drawing::Point(162, 594);
+			this->write_password->Margin = System::Windows::Forms::Padding(12, 12, 12, 12);
+			this->write_password->Name = L"write_password";
+			this->write_password->Size = System::Drawing::Size(500, 81);
+			this->write_password->TabIndex = 2;
+			this->write_password->text = L"password";
+			this->write_password->OnTextChange += gcnew System::EventHandler(this, &login::Write_Password);
+			// 
+			// write_Username
+			// 
+			this->write_Username->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(0)), static_cast<System::Int32>(static_cast<System::Byte>(0)),
+				static_cast<System::Int32>(static_cast<System::Byte>(64)));
+			this->write_Username->BackgroundImage = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"write_Username.BackgroundImage")));
+			this->write_Username->BackgroundImageLayout = System::Windows::Forms::ImageLayout::Stretch;
+			this->write_Username->ForeColor = System::Drawing::Color::MintCream;
+			this->write_Username->Icon = (cli::safe_cast<System::Drawing::Image^>(resources->GetObject(L"write_Username.Icon")));
+			this->write_Username->Location = System::Drawing::Point(162, 358);
+			this->write_Username->Margin = System::Windows::Forms::Padding(12, 12, 12, 12);
+			this->write_Username->Name = L"write_Username";
+			this->write_Username->Size = System::Drawing::Size(500, 81);
+			this->write_Username->TabIndex = 1;
+			this->write_Username->text = L"Username";
+			this->write_Username->OnTextChange += gcnew System::EventHandler(this, &login::Write_Username);
+			// 
+			// label1
+			// 
+			this->label1->AutoSize = true;
+			this->label1->Font = (gcnew System::Drawing::Font(L"Segoe UI Black", 27.75F, System::Drawing::FontStyle::Bold, System::Drawing::GraphicsUnit::Point,
+				static_cast<System::Byte>(0)));
+			this->label1->Location = System::Drawing::Point(288, 71);
+			this->label1->Margin = System::Windows::Forms::Padding(6, 0, 6, 0);
+			this->label1->Name = L"label1";
+			this->label1->Size = System::Drawing::Size(279, 99);
+			this->label1->TabIndex = 0;
+			this->label1->Text = L"LOGIN";
+			this->label1->Click += gcnew System::EventHandler(this, &login::Write_Password);
+			// 
 			// login
 			// 
-			this->AutoScaleDimensions = System::Drawing::SizeF(6, 13);
+			this->AutoScaleDimensions = System::Drawing::SizeF(12, 25);
 			this->AutoScaleMode = System::Windows::Forms::AutoScaleMode::Font;
 			this->BackColor = System::Drawing::Color::FromArgb(static_cast<System::Int32>(static_cast<System::Byte>(70)), static_cast<System::Int32>(static_cast<System::Byte>(70)),
 				static_cast<System::Int32>(static_cast<System::Byte>(82)));
-			this->ClientSize = System::Drawing::Size(1287, 699);
+			this->ClientSize = System::Drawing::Size(2564, 1344);
 			this->Controls->Add(this->panel1);
+			this->Cursor = System::Windows::Forms::Cursors::IBeam;
+			this->Margin = System::Windows::Forms::Padding(6, 6, 6, 6);
 			this->Name = L"login";
 			this->Text = L"login";
 			this->panel1->ResumeLayout(false);
